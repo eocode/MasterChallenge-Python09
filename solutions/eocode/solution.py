61f44aa5a06e2d498b861a52495e47d33a0b93c0
Elias Ojeda Medina <eliasojedamedina@gmail.com>
2020-05-25 12:09:21 -0500

    Correct var names with Python Style Guide

--- solutions/eocode/solution.py
+++ solutions/eocode/solution.py
@@ -7,31 +7,31 @@ class Solution:
 
     def duplicate_zeros(self, arr: List[int]):
         """Function that duplicates 0s without altering the memory used."""
-        zeros = 0
-        a_len = len(arr)
-        secureZone = -1
+        count_zeros = 0
+        array_len = len(arr)
+        secure_zone_to_write = -1
         for i, v in enumerate(arr):
             if v == 0:
-                secureZone = i if secureZone == -1 else secureZone
-                if i + zeros + 1 < a_len:
-                    zeros += 1
+                secure_zone_to_write = i if secure_zone_to_write == -1 else secure_zone_to_write
+                if i + count_zeros + 1 < array_len:
+                    count_zeros += 1
                 else:
                     break
-        b = a_len - 1
-        l = b - zeros
-        secureZone = secureZone + (zeros * 2)
-        while l >= 0:
-            if arr[l] != 0:
-                arr[b] = arr[l]
-                b -= 1
+        final_array_index = array_len - 1
+        index = final_array_index - count_zeros
+        secure_zone_to_write = secure_zone_to_write + (count_zeros * 2)
+        while index >= 0:
+            if arr[index] != 0:
+                arr[final_array_index] = arr[index]
+                final_array_index -= 1
             else:
-                if b == a_len-1 and (a_len - secureZone) % 2 == 1:
-                    arr[b] = 0
-                    b -= 1
-                    zeros -= 1
+                if final_array_index == array_len-1 and (array_len - secure_zone_to_write) % 2 == 1:
+                    arr[final_array_index] = 0
+                    final_array_index -= 1
+                    count_zeros -= 1
                 else:
-                    arr[b] = 0
-                    arr[b-1] = 0
-                    b -= 2
-                    zeros -= 1
-            l -= 1
+                    arr[final_array_index] = 0
+                    arr[final_array_index-1] = 0
+                    final_array_index -= 2
+                    count_zeros -= 1
+            index -= 1
